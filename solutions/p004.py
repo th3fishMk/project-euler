f19c817e76376d2cb9dc2880cec55d8b4dc1b01e
fish <th3fishmk@proton.me>
2025-09-13 02:08:50 -0600

    solved p004

---
+++ solutions/p004.py
@@ -0,0 +1,34 @@
+# problem 004
+
+prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+
+
+def find_prime_divisors(num):
+    i = 2
+    factors = []
+    while i * i <= num:
+        if num % i:
+            i += 1
+        else:
+            num //= i
+            factors.append(i)
+    if num > 1:
+        factors.append(num)
+    return factors
+
+
+smallest = 1
+for i in range(1, 21):
+    if (smallest % i == 0):
+        pass
+    elif i in prime_numbers:
+        smallest = smallest * i
+    else:
+        divs = find_prime_divisors(i)
+        divisible = False
+        for div in divs:
+            smallest = smallest*div
+            if smallest % i == 0:
+                break
+
+print(smallest)
